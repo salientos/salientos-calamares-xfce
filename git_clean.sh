b0c07a4d1d59ec6f0b36b893fb02b1f94a4abeb2
salientos <d3signr@gmail.com>
2021-02-15 08:50:17 +0000

    Update Build

--- git_clean.sh
+++ git_clean.sh
@@ -2,15 +2,15 @@
 
 ##############################################################################
 #
-#  Program :	Clean_Up v1
-#  Arch    :	x86_64 
-#  Author  : 	Silent Robot
-#  Website : 	https://sourceforge.net/projects/salient-os/
+#  Program :	git_clean v1
+#  Arch    :	x86_64
+#  Author  :	Silent Robot
+#  Website :	https://sourceforge.net/projects/salient-os/
 #
 ##############################################################################
 
 _msg() {
-	term_cols=$(tput cols) 
+	term_cols=$(tput cols)
 	str=":: $1 ::"
 	for ((i=1; i<=`tput cols`; i++)); do echo -n ‾; done
 	tput setaf 10; printf "%*s\n" $(((${#str}+$term_cols)/2)) "$str"; tput sgr0
